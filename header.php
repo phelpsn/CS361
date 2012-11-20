c4ccbeb793099d067dcf144e34ffcb2c841cc926
Sean Hammond <hammonse@onid.orst.edu>
2012-11-19 16:49:47 -0800

    changed index to pbp and added a header include

---
+++ header.php
@@ -0,0 +1,7 @@
+<a href="index.html"><img src="http://i.imgur.com/E0iWs.jpg"></a>
+<div id="menu">
+<ul>
+	<li><a href="index.php">Homepage</a></li>
+	<li><a href="test.php">Regions Map</a></li>
+	<li><a href = "application.php">Charities, apply here!</a></li>
+</div>
---
+++ index.php
@@ -0,0 +1,17 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<html>
+
+	<head>
+
+		<title>Hunger Connect/title>
+
+
+	</head>
+
+	<body>
+	<?php include 'header.php' ?>
+
+	</body>
+
+</html>
